cfee21f65eddd4eef7a07a6168a91b6b271538d2
Shoaib5999 <shoaibakram599@gmail.com>
2024-03-23 11:31:33 +0530

    all set for the production

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import { connectDB } from "./config/db.js";
 import authRoute from "./routes/authRoute.js";
-import categoryRoutes from "./routes/categoryRoutes.js"
-import productRoutes from "./routes/productRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 
@@ -19,15 +20,16 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join__dirname, "./client/build"));
 
 // Routes
 app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/category",categoryRoutes)
-app.use("/api/v1/products",productRoutes)
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/products", productRoutes);
 
 // APIs
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to our ecommerce website</h1>");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
